e1393e7c79dad3919bc5a3e8fffeb23e62cc35ef
Przemek Gałdys <galdys.przemek@gmail.com>
2018-01-28 16:22:52 +0100

    Mongo DB

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb://PrzemekG:qwerty12345@ds117178.mlab.com:17178/test1234', {
     useMongoClient: true
 });
 
